f62a95d0bdf0519fcd4b44aa14244634cc1e104f
igorrr234 <igor.oliveira0407silva@gmail.com>
2023-08-16 22:04:06 -0300

    segunda parte do crud

--- classes/Crud.php
+++ classes/Crud.php
@@ -45,8 +45,41 @@ class Crud{
         return $stmt;
     }
 
-} 
 
-
+    //função atualizar registros
+    public function update ($postValues){
+        $id = $postValues ['id'];
+        $modelo = $postValues ['modelo'];
+        $marca = $postValues ['marca'];
+        $placa = $postValues ['placa'];
+        $cor = $postValues ['cor'];
+        $ano = $postValues ['ano'];
+        
+        if (empty($id) || empty ($modelo) || empty ($marca) || empty ($placa) || empty ($cor) || empty ($ano)){
+            return false;
+        }
+        $query = "UPDATE ". $this->table_name . "SET modelo = ?, marca = ?, placa = ?, cor = ?, ano = ? WHERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt-> bindParam(1,$modelo);
+        $stmt-> bindParam(2,$marca);
+        $stmt-> bindParam(3,$placa);
+        $stmt-> bindParam(4,$cor);
+        $stmt-> bindParam(5,$ano);
+        $stmt-> bindParam(6,$id);
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+        //função para pegar os registros do banco e inserir no formulário
+        public function readOne($id){
+            $query = "SELECT * FROM ". $this->table_name . "WHERE id = ?";
+            $stmt = $this->conn->prepare($query);
+            $stmt-> bindParam(1,$id);
+            $stmt->execute();
+            return $stmt->fetch (PDO::FETCH_ASSOC);
+        }
+    }
 
 ?>
